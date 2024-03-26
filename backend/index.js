efc10522d009d7ff481eb4d85ecca80856d365cc
Pankaj Kumar <pankaj.ky3007@gmail.com>
2024-03-26 15:37:04 +0530

    changed deleted

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express=require("express");
 const cookieParser=require("cookie-parser");
 const cors=require("cors");
 const fileupload=require("express-fileupload");
-const bodyParser=require("body-parser");
 
 require("dotenv").config();
 
@@ -16,8 +15,7 @@ app.use(cors(
     }
 ));
 
-// app.use(express.json());
-app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cookieParser());
 
 const PORT=process.env.PORT || 4000;
